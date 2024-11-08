faadae18638c29302937eab6f7f1210b6a161d94
SHiTU23 <shi.tu2323@gmail.com>
2024-11-08 12:48:05 +0330

    test the class for keypoint extraction
    + check the out put when there is no bottle in the image

--- keypoint_detection/yolo/bottle_finder.py
+++ keypoint_detection/yolo/bottle_finder.py
@@ -4,8 +4,8 @@ import os
 
 class keypoint:
     def __init__(self):
-        _currect_dir = os.path.dirname(__file__)
-        _model_path = _currect_dir + '/best.pt'
+        _current_dir = os.path.dirname(__file__)
+        _model_path = _current_dir + '/best.pt'
         self.model = YOLO(_model_path)
 
         self.image_path = ''
@@ -27,73 +27,57 @@ class keypoint:
 
         _results = self.model(self.image_path)[0]
 
-        for result in _results:
-            for key_points in (result.keypoints.xy.tolist()):
-                _points.append(key_points)
-        self.bottle_keypoints['cap_corner'] = _points[0][0]
-        self.bottle_keypoints['pick_point'] = _points[0][1]
-        self.bottle_keypoints['angle_point'] = _points[0][2]
+        ### bottle found
+        if len(_results) > 0:
+            for result in _results:
+                for key_points in (result.keypoints.xy.tolist()):
+                    _points.append(key_points)
+            self.bottle_keypoints['cap_corner'] = _points[0][0]
+            self.bottle_keypoints['pick_point'] = _points[0][1]
+            self.bottle_keypoints['angle_point'] = _points[0][2]
 
-        return self.bottle_keypoints
+            return self.bottle_keypoints
+        ### No bottle found
+        else: 
+            return None 
 
     def show_image_with_keypoints(self):
-        for keypoint in self.bottle_keypoints.items:
-            self._image = cv2.imread(self.image_path)
+        self._image = cv2.imread(self.image_path)
+        if any(value != 0 for value in self.bottle_keypoints.values()):
+            for keypoint in self.bottle_keypoints.items():
+                ### put the key points on the image
+                keypoint_id = keypoint[0]
+                x, y = int(keypoint[1][0]), int(keypoint[1][1])
+                cv2.putText(self._image, f"{keypoint_id}: x:{x}, y:{y}", (x, y+2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                cv2.circle(self._image, (x, y), 2, (0, 0, 255), 3)
 
-            keypoint_id = keypoint[0]
-            x, y = int(keypoint[1][0]), int(keypoint[1][1])
-            cv2.putText(self._image, f"{keypoint_id}: x:{x}, y:{y}", (x, y+2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-            cv2.circle(self._image, (x, y), 2, (0, 0, 255), 3)
-            cv2.imshow("predicted image", self._image)
-            cv2.waitKey(0)
+        cv2.imshow("predicted image", self._image)
+        cv2.waitKey(0)
 
     def save_predicted_image(self, save_path, image_name):
         cv2.imwrite(f"{save_path}/{image_name}.jpg", self._image)
 
 
 '''
-
 ************************
 TO DO
-1. test what is the output when there is no bottle in the image
+1. test what is the output when there is no bottle in the image ----- DONE
 2. test for the camera images - real time
-3. test the whole class
+3. test the whole class    ----- DONE
 4. test the show funtion when using the camer - the waitkey part might be a problem
 
-
-image_name = '67_red.jpg_0_484.jpg'
-test_image_path = currect_dir + '/test_images/' + image_name
-save_path = currect_dir + '/predicted_images'
-
-image = cv2.imread(test_image_path)
-
-model = YOLO(model_path)
-results = model(test_image_path)[0]
-
-points = []
-bottle_keypoints = {'cap_corner':0, 'pick_point':0, 'angle_point':0}
-
-for result in results:
-    for key_points in (result.keypoints.xy.tolist()):
-        points.append(key_points)
-        for keypoint_indx, keypoint in enumerate(key_points):
-            x = int(keypoint[0])
-            y = int(keypoint[1])
-            cv2.putText(image, f"{keypoint_indx}: x:{x}, y:{y}", (x, y+2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-            cv2.circle(image, (x, y), 2, (0, 0, 255), 3)
-print(points)
-bottle_keypoints['cap_corner'] = points[0][0]
-bottle_keypoints['pick_point'] = points[0][1]
-bottle_keypoints['angle_point'] = points[0][2]
-
-print(f"bottle points: {bottle_keypoints}")
-
-cv2.imwrite(f"{save_path}/predicted_{image_name}.jpg", image)
-# cv2.imshow("image", image)
-
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 '''
-dict_hi = {'cap_corner':10, 'pick_point':50, 'angle_point':30}
-for key in dict_hi.items():
-    print(key[1])
+
+if __name__ == "__main__":
+    image_name = 'emptyscene.jpg'
+    # image_name = '67_red.jpg_0_1958.jpg'
+
+    current_dir = os.path.dirname(__file__)
+    image_path = current_dir + '/test_images/' + image_name
+    save_path = current_dir + '/predicted_images'
+
+    key_point_detector = keypoint()
+    result = key_point_detector.predict(image_path)
+    print(f"result is : {result}")
+
+    key_point_detector.show_image_with_keypoints()
