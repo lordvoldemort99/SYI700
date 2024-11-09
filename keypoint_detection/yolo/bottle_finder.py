efe9c8136ef12540dd055e3ede0d6b12c7782113
SHiTU23 <shi.tu2323@gmail.com>
2024-11-09 15:50:36 +0330

    Add: bottle color detecttion
    + a triangle of (pick point, topleft corner and topright corner of the cap is investigated for color
    + the color of majority of pixels in the triamgle is return

--- keypoint_detection/yolo/bottle_finder.py
+++ keypoint_detection/yolo/bottle_finder.py
@@ -1,6 +1,8 @@
 from ultralytics import YOLO
 import cv2
 import os
+import math
+import numpy as np
 
 class keypoint:
     def __init__(self):
@@ -22,6 +24,8 @@ class keypoint:
                   angle_point: coordinates of center point at the end of the bottle(for angle caculation)
         '''
         self._image = image_array
+        self._image_width, self._image_height = self._image.shape[0], self._image.shape[1]
+
         _points = []
         self.bottle_keypoints = {'cap_corner':0, 'pick_point':0, 'angle_point':0}
 
@@ -41,7 +45,72 @@ class keypoint:
         else: 
             return None 
 
-    def show_image_with_keypoints(self):
+    def bottle_data(self) -> list:
+        """
+            Returns:
+            [x, y, orientation, color]
+        """
+
+    def bottle_color(self) -> str:
+        """
+            Takes the cap point, creates a bounding box utilizing pick-point. 
+            Get average on the pixel color
+        """
+        ######################################################################################
+        ####    Create a point in front of top left corner on the cap, on the right side   ###
+        ######################################################################################
+
+        topLeft_x, topLeft_y = self.bottle_keypoints['cap_corner'][0], self.bottle_keypoints['cap_corner'][1]   
+        pickPoint_x, pickPoint_y = self.bottle_keypoints['pick_point'][0], self.bottle_keypoints['pick_point'][1]  
+
+        ### amplitude of the line between pick-point and topleft-corner on the cap
+        amplitude = math.sqrt((pickPoint_x - topLeft_x)**2 + (pickPoint_y - topLeft_y)**2) 
+
+        ### the angle of the line
+        theta = math.atan2((pickPoint_y-topLeft_y), (pickPoint_x-topLeft_x))
+        theta_degrees = theta * (180/np.pi)
+
+        ### the point on the top right corner of the cap
+        rotated_angle = (theta_degrees - 120) * (np.pi / 180) ## convert to Raduian
+
+        topRight_x = pickPoint_x + amplitude * math.cos(rotated_angle)
+        topRight_y = pickPoint_y + amplitude * math.sin(rotated_angle)
+
+        ############################################################################################################
+        ###  find the color of pixels in the triangle created from (pick-point, topLeft-corner, topRight-corner) ###
+        ############################################################################################################
+
+        vector1 = (int(topLeft_x), int(topLeft_y))
+        vector2 = (int(pickPoint_x), int(pickPoint_y))
+        vector3 = (int(topRight_x), int(topRight_y))
+
+        min_x = min(vector1[0], vector2[0], vector3[0])
+        max_x = max(vector1[0], vector2[0], vector3[0])
+
+        min_y = min(vector1[1], vector2[1], vector3[1])
+        max_y = max(vector1[1], vector2[1], vector3[1])
+
+        _pixel_colors = []
+        for x in range(min_x, max_x+1): 
+            for y in range(min_y, max_y+1):
+                if 0 <= x < self._image_width and 0 <= y < self._image_height and self._is_point_in_triangle(x, y, vector1, vector2, vector3):
+                    color= self._image[int(y), int(x)]
+                    _pixel_colors.append(color)
+
+        cap_color = self._color_majortity(_pixel_colors)
+        return cap_color
+
+    def bottle_orientation(self) -> float:
+        """
+            Calculate the angle using pick-point and angle-point
+            if negative, add 360; all angles are between 0 - 360
+        """
+
+    def show_image_with_keypoints(self, stream=True):
+        """
+            stream = True (default) : while real-time images from camera or video are being detected
+            stream = False : if only one picture is being detected and shown
+        """
         # self._image = cv2.imread(self.image)
         if any(value != 0 for value in self.bottle_keypoints.values()):
             for keypoint in self.bottle_keypoints.items():
@@ -52,10 +121,51 @@ class keypoint:
                 cv2.circle(self._image, (x, y), 2, (0, 0, 255), 3)
 
         cv2.imshow("predicted image", self._image)
-        # cv2.waitKey(0)
+        if stream == False:
+            cv2.waitKey(0)
 
     def save_predicted_image(self, save_path, image_name):
         cv2.imwrite(f"{save_path}/{image_name}.jpg", self._image)
+
+    def _is_point_in_triangle(self, x, y, v1, v2, v3):
+    # Using barycentric coordinates method to check if (x, y) lies inside the triangle
+        def sign(p1, p2, p3):
+            # Calculates the signed area of the triangle formed by p1, p2, and p3
+            return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
+
+        # all of these should have the same sign
+        b1 = sign((x, y), v1, v2) < 0.0
+        b2 = sign((x, y), v2, v3) < 0.0
+        b3 = sign((x, y), v3, v1) < 0.0
+
+        # Check if all signs are the same (either all True or all False)
+        return (b1 == b2) and (b2 == b3)
+
+    def _color_detection(self, pixel_color):
+        blue, green, red = pixel_color
+
+        if red > blue and green > blue:
+            return 'yellow'
+        elif red > blue and red > green:
+            return 'red'
+        elif blue > red and blue > green:
+            return 'blue'
+
+    def _color_majortity(self, pixels_color_list):
+        colors_counters = {'blue' : 0, 'yellow' : 0, 'red' : 0}
+        
+        for pixel in pixels_color_list:
+            color = self._color_detection(pixel)
+            if color == 'blue':
+                colors_counters['blue'] += 1
+            elif color == 'yellow':
+                colors_counters['yellow'] += 1
+            elif color == 'red':
+                colors_counters['red'] += 1
+
+        return max(colors_counters, key=colors_counters.get)
+
+
 
 
 '''
@@ -69,18 +179,23 @@ TO DO
 4. test the show funtion when using the camer - the waitkey part might be a problem
 
 '''
-
 if __name__ == "__main__":
-    image_name = 'emptyscene.jpg'
-    # image_name = '67_red.jpg_0_1958.jpg'
+    # image_name = 'emptyscene.jpg'
+    # image_name = '67_red.jpg_0_1958.jpg'    ## head  up  to the right - red
+    # image_name = '36_Videoimages.jpg'       ## head  up  to the left - red
+    # image_name = '70_blue.jpg_0_9550.jpg'   ## head down to the right - blue
+    # image_name = '4_yellow.jpg_0_6674.jpg'  ## head down to the right - yellow
+    image_name = 'yellow_Videoimages.jpg'   ## head up to the right - yellow
 
     current_dir = os.path.dirname(__file__)
     image_path = current_dir + '/test_images/' + image_name
     save_path = current_dir + '/predicted_images'
 
     image = cv2.imread(image_path)
+    image_width, image_height = image.shape[0], image.shape[1]
     key_point_detector = keypoint()
     result = key_point_detector.predict(image)
     print(f"result is : {result}")
-
-    key_point_detector.show_image_with_keypoints()
+    bottle_color = key_point_detector.bottle_color()
+    print(f"color of the bottle: {bottle_color}")
+    key_point_detector.show_image_with_keypoints(stream=False)
