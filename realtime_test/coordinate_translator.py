a6b4a58c1e413dac53706068fd937d7dc9466bb8
SHiTU23 <shi.tu2323@gmail.com>
2024-11-13 17:27:53 +0330

    Refactor: separate arucoDetection from converting coords into real dim
    /realtime_test/coordinate_translator.py

--- realtime_test/coordinate_translator.py
+++ realtime_test/coordinate_translator.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 
-NO_ARUCO_FOUND = -1
+NO_ARUCO_FOUND = None
 
 class translator:
     def __init__(self, aruco_dictionary, aruco_length):
@@ -13,70 +13,88 @@ class translator:
         """
         self.aruco_area = 4 * aruco_length
         self.aruco_marker = aruco_dictionary
+        self._aruco_corners = NO_ARUCO_FOUND
     
-    def translate_coordinate(self, image, coordinates, angle):
+    def aruco_detector(self, image):
+        self._image = image
+        self._aruco_corners = []
+        _parameters = cv2.aruco.DetectorParameters()
+        _aruco_dict = cv2.aruco.getPredefinedDictionary(self.aruco_marker)
+        _aruco_detector = cv2.aruco.ArucoDetector(_aruco_dict, _parameters)
+        _corners, ids, _ = _aruco_detector.detectMarkers(image)
+
+        if ids is not NO_ARUCO_FOUND:
+            self._aruco_corners = _corners
+            int_corners = np.int32(self._aruco_corners) 
+
+            aruco_perimeter = cv2.arcLength(self._aruco_corners[0], True)
+
+            self._pixel_cm_ratio = aruco_perimeter / self.aruco_area
+
+            self._aruco_top_left_x = int_corners[0][0][0][0] ### The point marked by drawDetectedMarkers
+            self._aruco_top_left_y = int_corners[0][0][0][1]
+
+            self._aruco_top_right_x = int_corners[0][0][1][0] ### the point in front of the marked point from the drawDetectedMarkers
+            self._aruco_top_right_y = int_corners[0][0][1][1]
+
+            self._aruco_bottom_right_x = int_corners[0][0][2][0] 
+            self._aruco_bottom_right_y = int_corners[0][0][2][1]
+
+            self._aruco_bottom_left_x = int_corners[0][0][3][0]
+            self._aruco_bottom_left_x = int_corners[0][0][3][1]
+
+            self.show_aruco()
+        else:
+            self._aruco_corners = NO_ARUCO_FOUND
+            
+        return self._aruco_corners 
+
+    def translate_coordinates(self, coordinates, orientation):
         old_x, old_y = coordinates
+        old_angle = orientation
 
-        font_color = {'red':(0, 0, 255), 'green': (0, 255, 0),'orange':(0, 100, 200), 'black':(0,0,0)}
+        if self._aruco_corners is not NO_ARUCO_FOUND:
+            ### calculat the orientation of the camera
+            aruco_vector = np.array([self._aruco_top_right_x - self._aruco_top_left_x, self._aruco_top_right_y - self._aruco_top_left_y]) ### x direction
+            image_vector = np.array([1,0])  ### (x, y)
 
-        parameters = cv2.aruco.DetectorParameters()
-        aruco_dict = cv2.aruco.getPredefinedDictionary(self.aruco_marker)
-        aruco_detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
-        corners, ids, _ = aruco_detector.detectMarkers(image)
+            print(f"top_left: {self._aruco_top_left_x}, {self._aruco_top_left_y}; top_right: {self._aruco_top_right_x}, {self._aruco_top_right_y}")
+            tfangle = np.arccos((np.dot(aruco_vector, image_vector)/ (np.linalg.norm(aruco_vector)*np.linalg.norm(image_vector))))   ## cos(theta)=A.B / |A|x|B|
+            
+            if self._aruco_top_right_y < self._aruco_top_left_y:
+                tfangle = -tfangle
+            ### tfangle is in radian
+            print(f"the angle between aruco upper side and image top line is {np.degrees(tfangle)}")
 
-        if ids is not None:
-            cv2.aruco.drawDetectedMarkers(image, corners, ids)
-            if corners:
-                int_corners = np.int32(corners) 
-
-                aruco_perimeter = cv2.arcLength(corners[0], True)
-                pixel_cm_ratio = aruco_perimeter / self.aruco_area
-
-                aruco_top_left_x = int_corners[0][0][0][0] ### The point marked by drawDetectedMarkers
-                aruco_top_left_y = int_corners[0][0][0][1]
-
-                aruco_top_right_x = int_corners[0][0][1][0] ### the point in front of the marked point from the drawDetectedMarkers
-                aruco_top_right_y = int_corners[0][0][1][1]
-
-                aruco_bottom_right_x = int_corners[0][0][2][0] 
-                aruco_bottom_right_y = int_corners[0][0][2][1]
-
-                aruco_bottom_left_x = int_corners[0][0][3][0]
-                aruco_bottom_left_x = int_corners[0][0][3][1]
-
-                cv2.circle(image, (aruco_top_right_x, aruco_top_right_y), 2, font_color['orange'], 2)
-                cv2.putText(image, 'topRight', (aruco_top_right_x, aruco_top_right_y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, font_color['orange'], 2)
-
-                cv2.circle(image, (aruco_top_left_x, aruco_top_left_y), 2, font_color['orange'], 2)
-                cv2.putText(image, 'topLeft', (aruco_top_left_x, aruco_top_left_y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, font_color['orange'], 2)
-
-                cv2.circle(image, (aruco_bottom_right_x, aruco_bottom_right_y), 2, font_color['orange'], 2)
-                cv2.putText(image, 'topLeft', (aruco_bottom_right_x, aruco_bottom_right_y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, font_color['orange'], 2)
-
-                ### calculat the orientation of the camera
-                aruco_vector = np.array([aruco_top_right_x - aruco_top_left_x, aruco_top_right_y - aruco_top_left_y]) ### x direction
-                image_vector = np.array([1,0])  ### (x, y)
-
-                print(f"top_left: {aruco_top_left_x}, {aruco_top_left_y}; top_right: {aruco_top_right_x}, {aruco_top_right_y}")
-                tfangle = np.arccos((np.dot(aruco_vector, image_vector)/ (np.linalg.norm(aruco_vector)*np.linalg.norm(image_vector))))   ## cos(theta)=A.B / |A|x|B|
-                
-                if aruco_top_right_y < aruco_top_left_y:
-                    tfangle = -tfangle
-                ### tfangle is in radian
-                print(f"the angle between aruco upper side and image top line is {np.degrees(tfangle)}")
-
-                tfmatrix1 = np.matrix([[np.cos(tfangle), -np.sin(tfangle), 0 , aruco_top_left_x],
-                                       [np.sin(tfangle),  np.cos(tfangle), 0 , aruco_top_left_y],
-                                       [       0       ,         0       , 1 ,        0        ],
-                                       [       0       ,         0       , 0 ,        1        ]])
-                
-                old_coordinate = np.matrix([[old_x],[old_y],[0],[1]])
-                new_coordinate = np.matmul(np.linalg.inv(tfmatrix1),old_coordinate)
-                new_coordinate = new_coordinate/pixel_cm_ratio
-                new_angle = angle + np.degrees(tfangle)
-                return new_coordinate, new_angle
+            tfmatrix1 = np.matrix([[np.cos(tfangle), -np.sin(tfangle), 0 , self._aruco_top_left_x],
+                                    [np.sin(tfangle),  np.cos(tfangle), 0 , self._aruco_top_left_y],
+                                    [       0       ,         0       , 1 ,        0        ],
+                                    [       0       ,         0       , 0 ,        1        ]])
+            
+            old_coordinate = np.matrix([[old_x],[old_y],[0],[1]])
+            new_coordinate = np.matmul(np.linalg.inv(tfmatrix1),old_coordinate)
+            new_coordinate = new_coordinate/self._pixel_cm_ratio
+            new_angle = old_angle + np.degrees(tfangle)
+            return new_coordinate, new_angle
         else:
             return NO_ARUCO_FOUND
+
+    def show_aruco(self):
+        if self._aruco_corners is not NO_ARUCO_FOUND:
+            cv2.aruco.drawDetectedMarkers(self._image, self._aruco_corners)
+    def show_aruco_corners(self):
+        font_color = {'red':(0, 0, 255), 'green': (0, 255, 0),'orange':(0, 100, 200), 'black':(0,0,0)}
+
+        if self._aruco_corners is not NO_ARUCO_FOUND:
+            cv2.circle(self._image, (self._aruco_top_right_x, self._aruco_top_right_y), 2, font_color['orange'], 2)
+            cv2.putText(self._image, 'topRight', (self._aruco_top_right_x, self._aruco_top_right_y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, font_color['orange'], 2)
+
+            cv2.circle(self._image, (self._aruco_top_left_x, self._aruco_top_left_y), 2, font_color['orange'], 2)
+            cv2.putText(self._image, 'topLeft', (self._aruco_top_left_x, self._aruco_top_left_y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, font_color['orange'], 2)
+
+            cv2.circle(self._image, (self._aruco_bottom_right_x, self._aruco_bottom_right_y), 2, font_color['orange'], 2)
+            cv2.putText(self._image, 'topLeft', (self._aruco_bottom_right_x, self._aruco_bottom_right_y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, font_color['orange'], 2)
+
 
 
 
@@ -88,10 +106,13 @@ if __name__ == "__main__":
     angle = 20
 
     aruco_length = 100
-    translator = translator(aruco_dictionary=cv2.aruco.DICT_5X5_100, aruco_length=aruco_length)
-    translator_result = translator.translate_coordinate(image, old_coordinate, angle)
+    coord_translator = translator(aruco_dictionary=cv2.aruco.DICT_5X5_100, aruco_length=aruco_length)
+    coord_translator.aruco_detector(image)
+    translation_result = coord_translator.translate_coordinates(old_coordinate, angle)
 
-    if translator_result is NO_ARUCO_FOUND:
+    if translation_result is NO_ARUCO_FOUND:
         print("NO ARUCO FOUND !!")
     else:
-        print(f"new coordinates: {translator_result}")
+        print(f"new coordinates: {translation_result}")
+        cv2.imshow('image', image)
+        cv2.waitKey(0)
