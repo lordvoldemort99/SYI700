8499e1239ce9c50c21257117f153888e641cc0ee
SHiTU23 <shi.tu2323@gmail.com>
2024-11-15 21:36:00 +0330

    Add: run the detection every 0.5 sec

--- realtime_test/main.py
+++ realtime_test/main.py
@@ -19,10 +19,10 @@ ARUCO_MARKER = cv2.aruco.DICT_5X5_100
 
 ### these have been found by avaluating by measuring in the real layout
 X_OFFSET = 20
-Y_OFFSET = 10
+Y_OFFSET = 20
 
 current_dir = os.path.dirname(__file__)
-predictedImg_savePath = current_dir + '/predicted_images/' 
+savePath = current_dir + '/predicted_images' 
 
 translator = coordinate_translator.translator(ARUCO_MARKER, ARUCO_LENGTH)
 
@@ -55,25 +55,26 @@ while camera.IsGrabbing():
         image = image.GetArray()
         image = cv2.resize(image, (frame_width, frame_height))
 
-        aruco_state = translator.aruco_detector(image)
-        bottle_data = keypoint_detector.bottle_features(image)
-        if bottle_data != None:
-            pick_point = tuple(bottle_data[:2])
-            bottle_orientation = bottle_data[2]
-            print("bottle key-points: ", bottle_data)
-            print(f"pick point: {pick_point}")
+        if new_time - last_time > 0.5:
+            aruco_state = translator.aruco_detector(image)
+            bottle_data = keypoint_detector.bottle_features(image)
+            if bottle_data != None:
+                pick_point = tuple(bottle_data[:2])
+                bottle_orientation = bottle_data[2]
+                print("bottle key-points: ", bottle_data)
+                print(f"pick point: {pick_point}")
 
-            if aruco_state != coordinate_translator.NO_ARUCO_FOUND:
-                new_coordinates, new_orientation = translator.translate_coordinates(pick_point, bottle_orientation)
-                new_coordinates = [coords[0] for coords in new_coordinates.tolist()]
-                new_coordinates = (new_coordinates[:2])
-                new_coordinates[0] = new_coordinates[0] - X_OFFSET
-                new_coordinates[1] = new_coordinates[1] - Y_OFFSET
-            
-                print("new_coordinates: ", new_coordinates, "real_angle: ", new_orientation) ### new_coordinates in mm and angle in degrees
+                if aruco_state != coordinate_translator.NO_ARUCO_FOUND:
+                    new_coordinates, new_orientation = translator.translate_coordinates(pick_point, bottle_orientation)
+                    new_coordinates = [coords[0] for coords in new_coordinates.tolist()]
+                    new_coordinates = (new_coordinates[:2])
+                    new_coordinates[0] = new_coordinates[0] - X_OFFSET
+                    new_coordinates[1] = new_coordinates[1] - Y_OFFSET
+                
+                    print("new_coordinates: ", new_coordinates, "real_angle: ", new_orientation) ### new_coordinates in mm and angle in degrees
 
-                cv2.putText(image, (f"real_pick_coords: ({int(new_coordinates[0])}, {int(new_coordinates[1])})"), (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
-        
+                    cv2.putText(image, (f"real_pick_coords: ({int(new_coordinates[0])}, {int(new_coordinates[1])})"), (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
+            last_time = new_time
         # translator.show_aruco()
         keypoint_detector.show_image_with_keypoints()
         # cv2.imshow("image", image)
@@ -87,7 +88,7 @@ while camera.IsGrabbing():
             break
         elif key == ord('s'):
             image_counter += 1
-            keypoint_detector.save_predicted_image(predictedImg_savePath, f'prediction_{image_counter}.jpg')
+            cv2.imwrite(f'{savePath}/image_{image_counter}.jpg', keypoint_detector._image)
             
             print(f"********** Image {image_counter} Saved **********")
         elif key == ord('r'):
