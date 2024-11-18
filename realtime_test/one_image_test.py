91714a4f482486e61413d933cf7c8e15f10aba60
SHiTU23 <shi.tu2323@gmail.com>
2024-11-18 14:56:41 +0330

    Add: seding data to MQTT

--- realtime_test/one_image_test.py
+++ realtime_test/one_image_test.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import time
 import sys
+from MV_MQTT import update_coordinates
 
 import coordinate_translator
 
@@ -26,15 +27,30 @@ image_path = current_dir + '/3_ArUco.jpg'
 image = cv2.imread(image_path)
 
 bottle_data = keypoint_detector.bottle_features(image)
+
+bottle_color = bottle_data[-1]
 pick_point = tuple(bottle_data[:2])
 bottle_orientation = bottle_data[2]
+
 print("bottle key-points: ", bottle_data)
-print(f"pick point: {pick_point}")
+# print(f"pick point: {pick_point}")
+# print(f"bottle_color: {bottle_color}")
 
-new_coordinates, new_orientation = translator.translate_coordinate(image, pick_point, bottle_orientation)
-new_coordinates = [coords[0] for coords in new_coordinates.tolist()]
-new_coordinates = (new_coordinates[:2])
-print(new_coordinates[0])
-print("new_coordinates: ", new_coordinates, "real_angle: ", new_orientation) ### new_coordinates in mm and angle in degrees
+aruco = translator.aruco_detector(image)
+if aruco != coordinate_translator.NO_ARUCO_FOUND:
 
+    new_coordinates, new_orientation = translator.translate_coordinates(pick_point, bottle_orientation)
+    new_coordinates = [coords[0] for coords in new_coordinates.tolist()]
+    new_coordinates = (new_coordinates[:2])
+    print(new_coordinates[0])
+    print("new_coordinates: ", new_coordinates, "real_angle: ", new_orientation) ### new_coordinates in mm and angle in degrees
+
+bottle_x = round(new_coordinates[0])
+bottle_y = round(new_coordinates[1])
+botte_angle = round(new_orientation)
+
+update_coordinates("update_variable", bottle_x, bottle_y,botte_angle, bottle_color)
+
+bottle_data = f"{bottle_x},{bottle_y},{botte_angle},{bottle_color}"
+print(f"bottleData: {bottle_data}")
 keypoint_detector.show_image_with_keypoints(video_stream=False)
